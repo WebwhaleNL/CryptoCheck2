f5e858f341ffa608103438fcbdd575d918a9badc
WebwhaleNL <74208369+WebwhaleNL@users.noreply.github.com>
2021-03-08 18:44:26 +0100

    requpdate2

--- crypto/settings.py
+++ crypto/settings.py
@@ -24,13 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'j1=_ke(ky7u)q5ptbqxer#ht3@9lv^6ypto*f(0x4qbp*-#w0$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 
 ALLOWED_HOSTS = [
-    'localhost',
-    '*'
+    'localhost'
 ]
 
 
@@ -125,4 +124,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+#STATIC_URL = '/static/'
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
